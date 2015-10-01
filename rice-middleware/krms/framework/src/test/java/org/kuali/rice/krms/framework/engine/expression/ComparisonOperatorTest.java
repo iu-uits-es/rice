87057fa693c62938800fb8360ab87f69b0a8c7c7
rojlarge <rojlarge@iu.edu>
2015-10-01 15:21:18 -0400

    Backport of several small KualiCo kc-rice fixes

--- rice-middleware/krms/framework/src/test/java/org/kuali/rice/krms/framework/engine/expression/ComparisonOperatorTest.java
+++ rice-middleware/krms/framework/src/test/java/org/kuali/rice/krms/framework/engine/expression/ComparisonOperatorTest.java
@@ -96,15 +96,15 @@ public class ComparisonOperatorTest {
     public void testExists() {
         ComparisonOperator op = ComparisonOperator.fromCode(ComparisonOperator.EXISTS.toString());
         op.setComparisonOperatorService(ComparisonOperatorServiceImpl.getInstance());
-        assertTrue(op.compare("123", "0"));
-        assertFalse(op.compare("123", null));
+        assertTrue(op.compare("123", null));
+        assertFalse(op.compare(null, null));
     }
 
     @Test
     public void testDoesNotExists() {
         ComparisonOperator op = ComparisonOperator.fromCode(ComparisonOperator.DOES_NOT_EXIST.toString());
         op.setComparisonOperatorService(ComparisonOperatorServiceImpl.getInstance());
-        assertFalse(op.compare("123", "0"));
-        assertTrue(op.compare("123", null));
+        assertFalse(op.compare("123", null));
+        assertTrue(op.compare(null, null));
     }
 }
