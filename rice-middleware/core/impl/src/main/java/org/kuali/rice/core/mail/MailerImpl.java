08d564eb56bc3351f902ac72b140f8326ba093eb
Andy Hill <athill@iu.edu>
2018-06-26 14:00:25 -0400

    ESIMW-1001: Update iu-rice to use digital email signing library
    * ./pom.xml: Add javamail-smime-transport.version
    * MailerImpl: Remove email signing code
    * rice-code-impl/pom.xml: Add javamail-smime-transport dependency

--- rice-middleware/core/impl/src/main/java/org/kuali/rice/core/mail/MailerImpl.java
+++ rice-middleware/core/impl/src/main/java/org/kuali/rice/core/mail/MailerImpl.java
@@ -15,104 +15,38 @@
  */
 package org.kuali.rice.core.mail;
 
-import org.apache.commons.lang.StringUtils;
-import org.bouncycastle.asn1.ASN1EncodableVector;
-import org.bouncycastle.asn1.cms.AttributeTable;
-import org.bouncycastle.asn1.cms.IssuerAndSerialNumber;
-import org.bouncycastle.asn1.smime.SMIMECapabilitiesAttribute;
-import org.bouncycastle.asn1.smime.SMIMECapability;
-import org.bouncycastle.asn1.smime.SMIMECapabilityVector;
-import org.bouncycastle.asn1.smime.SMIMEEncryptionKeyPreferenceAttribute;
-import org.bouncycastle.asn1.x500.X500Name;
-import org.bouncycastle.mail.smime.SMIMEException;
-import org.bouncycastle.mail.smime.SMIMESignedGenerator;
-import org.kuali.rice.core.api.config.property.ConfigContext;
-import org.kuali.rice.core.api.mail.EmailBcList;
-import org.kuali.rice.core.api.mail.EmailBody;
-import org.kuali.rice.core.api.mail.EmailCcList;
-import org.kuali.rice.core.api.mail.EmailFrom;
-import org.kuali.rice.core.api.mail.EmailSubject;
-import org.kuali.rice.core.api.mail.EmailTo;
-import org.kuali.rice.core.api.mail.EmailToList;
-import org.kuali.rice.core.api.mail.MailMessage;
-import org.kuali.rice.core.api.mail.Mailer;
-import org.springframework.beans.factory.InitializingBean;
+import org.kuali.rice.core.api.mail.*;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import javax.activation.CommandMap;
 import javax.activation.DataHandler;
-import javax.activation.MailcapCommandMap;
 import javax.mail.Address;
-import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import javax.mail.util.ByteArrayDataSource;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.Security;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertStore;
-import java.security.cert.CertStoreException;
-import java.security.cert.Certificate;
-import java.security.cert.CollectionCertStoreParameters;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
 
 /**
  * Maintains a Java Mail session and is used for sending e-mails.
  *
  * @author Kuali Rice Team (rice.collab@kuali.org)
  */
-public class MailerImpl implements Mailer, InitializingBean {
+public class MailerImpl implements Mailer {
 
     protected final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(MailerImpl.class);
 
     private JavaMailSenderImpl mailSender;
-    private KeyStore keyStore;
-    private String keyStoreFile;
-    private String keyStorePassword;
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(new FileInputStream(keyStoreFile), keyStorePassword.toCharArray());
-    }
 
     /**
      * @param mailSender The injected Mail Sender.
      */
     public void setMailSender(JavaMailSenderImpl mailSender) {
         this.mailSender = mailSender;
-    }
-
-    /**
-     * @param keyStoreFile The location on the filesystem where the keystore file is located
-     */
-    public void setKeyStoreFile(String keyStoreFile) {
-        this.keyStoreFile = keyStoreFile;
-    }
-
-    /**
-     * @param keyStorePassword The password used to unlock the keystore
-     */
-    public void setKeyStorePassword(String keyStorePassword) {
-        this.keyStorePassword = keyStorePassword;
     }
 
     /**
@@ -281,15 +215,6 @@ public class MailerImpl implements Mailer, InitializingBean {
             }
         }
 
-        Enumeration<String> aliases = keyStore.aliases();
-        while (aliases.hasMoreElements()) {
-            String alias = aliases.nextElement();
-            String keyPassword = ConfigContext.getCurrentContextConfig().getProperty(String.format("keystore.%s.password", StringUtils.substringBefore(alias, "@")));
-            if (alias.equals(from) && StringUtils.isNotBlank(keyPassword)) {
-                message = signMessage(message, alias, keyPassword);
-            }
-        }
-
         // Send the message
         try {
             mailSender.send(message);
@@ -298,93 +223,6 @@ public class MailerImpl implements Mailer, InitializingBean {
             LOG.error("sendMessage(): ", e);
             throw new RuntimeException(e);
         }
-    }
-
-    protected MimeMessage signMessage(final MimeMessage message, final String alias, final String keyPassword) {
-        try {
-            PrivateKey privateKey = (PrivateKey)keyStore.getKey(alias, keyPassword.toCharArray());
-            // Load certificate chain
-            Certificate[] chain = keyStore.getCertificateChain(alias);
-
-            MailcapCommandMap mailcap = (MailcapCommandMap) CommandMap.getDefaultCommandMap();
-
-            mailcap.addMailcap("application/pkcs7-signature;; x-java-content-handler=org.bouncycastle.mail.smime.handlers.pkcs7_signature");
-            mailcap.addMailcap("application/pkcs7-mime;; x-java-content-handler=org.bouncycastle.mail.smime.handlers.pkcs7_mime");
-            mailcap.addMailcap("application/x-pkcs7-signature;; x-java-content-handler=org.bouncycastle.mail.smime.handlers.x_pkcs7_signature");
-            mailcap.addMailcap("application/x-pkcs7-mime;; x-java-content-handler=org.bouncycastle.mail.smime.handlers.x_pkcs7_mime");
-            mailcap.addMailcap("multipart/signed;; x-java-content-handler=org.bouncycastle.mail.smime.handlers.multipart_signed");
-
-            // Create the SMIMESignedGenerator
-            SMIMECapabilityVector capabilities = new SMIMECapabilityVector();
-            capabilities.addCapability(SMIMECapability.dES_EDE3_CBC);
-            capabilities.addCapability(SMIMECapability.rC2_CBC, 128);
-            capabilities.addCapability(SMIMECapability.dES_CBC);
-            capabilities.addCapability(SMIMECapability.aES256_CBC);
-
-            Session session = Session.getDefaultInstance(mailSender.getJavaMailProperties(),
-                    new Authenticator() {
-                        @Override
-                        protected PasswordAuthentication getPasswordAuthentication() {
-                            return new PasswordAuthentication(alias, keyPassword);
-                        }
-                    });
-
-            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
-
-            ASN1EncodableVector attributes = new ASN1EncodableVector();
-            X500Name x500Name =  new X500Name(((X509Certificate) chain[0])
-                    .getIssuerDN().getName());
-            IssuerAndSerialNumber issuerAndSerialNumber = new IssuerAndSerialNumber(x500Name, ((X509Certificate) chain[0]).getSerialNumber());
-            attributes.add(new SMIMEEncryptionKeyPreferenceAttribute(issuerAndSerialNumber));
-            attributes.add(new SMIMECapabilitiesAttribute(capabilities));
-
-            SMIMESignedGenerator signer = new SMIMESignedGenerator();
-            signer.addSigner(
-                    privateKey,
-                    (X509Certificate) chain[0],
-                    "DSA".equals(privateKey.getAlgorithm()) ? SMIMESignedGenerator.DIGEST_SHA1
-                            : SMIMESignedGenerator.DIGEST_MD5,
-                    new AttributeTable(attributes), null);
-
-            // Add the list of certs to the generator
-            List<Certificate> certList = new ArrayList<Certificate>();
-            certList.add(chain[0]);
-            CertStore certs = CertStore.getInstance("Collection",
-                    new CollectionCertStoreParameters(certList), "BC");
-            signer.addCertificatesAndCRLs(certs);
-
-            // Sign the message
-            MimeMultipart mm = signer.generate(message, "BC");
-            MimeMessage signedMessage = new MimeMessage(session);
-
-            // Set all original MIME headers in the signed message
-            Enumeration headers = message.getAllHeaderLines();
-            while (headers.hasMoreElements()) {
-                signedMessage.addHeaderLine((String) headers.nextElement());
-            }
-
-            // Set the content of the signed message
-            signedMessage.setContent(mm);
-            return signedMessage;
-        } catch (KeyStoreException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (NoSuchAlgorithmException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (UnrecoverableKeyException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (NoSuchProviderException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (SMIMEException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (CertStoreException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (InvalidAlgorithmParameterException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        } catch (MessagingException e) {
-            LOG.error("Exception while signing message. Message will be sent unsigned", e);
-        }
-
-        return message;
     }
 
     protected void prepareHtmlMessage(String messageText, Message message) throws MessagingException {
